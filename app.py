6d039650206ecefeba09d6a28aa7bfbc5f9251aa
Enjala Pranaya <126013015@sastra.ac.in>
2025-08-01 14:21:10 +0530

    Final resume builder with styled resume and download

--- app.py
+++ app.py
@@ -1,24 +1,29 @@
-from flask import Flask, render_template, request
-from datetime import datetime
+from flask import Flask, render_template, request, send_file
+from werkzeug.utils import secure_filename
 import os
+from datetime import datetime
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'static/uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route('/')
-def form():
+def home():
     return render_template('form.html')
 
 @app.route('/resume', methods=['POST'])
 def resume():
-    data = request.form.to_dict(flat=False)
+    data = request.form.to_dict()
     photo = request.files['photo']
-    if photo and photo.filename:
-        photo_path = os.path.join('static', photo.filename)
-        photo.save(photo_path)
-        data['photo_path'] = photo_path
+    if photo:
+        filename = secure_filename(photo.filename)
+        filepath = os.path.join(UPLOAD_FOLDER, filename)
+        photo.save(filepath)
+        data['photo_path'] = filepath
     else:
         data['photo_path'] = None
-    return render_template('resume_template.html', data=data, datetime=datetime)
+
+    return render_template('resume_template.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
